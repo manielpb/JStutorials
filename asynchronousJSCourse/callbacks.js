2e7da80e48514da97955b0c50ad689d94230b4c8
maniel <manuelpepr99@icloud.com>
2024-01-23 01:27:36 +0000

    JS callbacks

---
+++ asynchronousJSCourse/callbacks.js
@@ -0,0 +1,33 @@
+let stocks = {
+    Fruit: ["strawberry", "grapes","banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone","cup","stick"],
+    toppings: ["chocolate", "peanuts"],
+};
+
+let order =(fruit_name, call_production) => {
+    setTimeout ( ()=> {console.log(`${stocks.Fruit[1]} was selected`)
+    call_production()
+}, 2000)
+}
+
+let production = ()=> {
+   setTimeout( ()=> {console.log("production has started")
+   setTimeout( ()=> {console.log(`${stocks.Fruit[1]} has been chopped` )
+   setTimeout ( ()=> {console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} has been added`)
+   setTimeout ( ()=> {console.log("start the machine")
+   setTimeout ( ()=> {console.log(`${stocks.holder[2]} was selected`)
+   setTimeout( ()=> {console.log(`${stocks.toppings[1]} was selected`)
+   setTimeout ( ()=> {console.log("ice cream has been selected")},2000)
+},3000)
+},2000)
+},1000)
+},1000)
+},2000)
+} ,0000)
+};
+
+order("", production);
+
+// when using callbacks remember to nest the the setTimeout() functions into each other 
+ß
